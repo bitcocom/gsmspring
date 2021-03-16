c8f60a1441822182670ad306187a3ec15e57f011
GSM <GSM@DESKTOP-UUM6D3I>
2021-03-16 14:57:12 +0900

    GSM

---
+++ gsmspring/src/kr/gsm/controller/MemberInsertController.java
@@ -0,0 +1,21 @@
+package kr.gsm.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/memberInsert.do")
+public class MemberInsertController extends HttpServlet {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. 파라메터수집(VO,DTO)
+		String id=request.getParameter("id");
+		String pass=request.getParameter("pass");
+		String name=request.getParameter("name");
+		int age=Integer.parseInt(request.getParameter("age"));
+		String phone=request.getParameter("phone");
+		String email=request.getParameter("email");
+		
+	}
+}
